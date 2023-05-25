0860f2971b831cf31aa6c86c3bf6262e73327aa9
Konrad Lang <konrad.lang@bbmri-eric.eu>
2023-05-25 11:28:04 +0200

    Updated Service Provider (Biobank) and Service (Collection) display, initial columns displayed and duplicates in display removed

--- src/config/initialBiobankColumns.js
+++ src/config/initialBiobankColumns.js
@@ -1,9 +1,8 @@
 const initialBiobankColumns = [
   { label: 'Id:', column: 'id', type: 'string', showCopyIcon: true },
-  { label: 'Description:', column: 'description', type: 'longtext' },
+  { label: 'Description:', column: 'description', type: 'longtext', showOnBiobankCard: true },
   { label: 'Name of Institute:', column: 'name_of_institue', type: 'string', showOnBiobankCard: true },
   { label: 'Acronym:', column: 'acronym', type: 'string', showOnBiobankCard: true },
-  { label: 'Description:', column: 'description', type: 'longtext', showOnBiobankCard: true },
   { label: 'URL:', column: 'url', type: 'hyperlink', showOnBiobankCard: true },
   // { label: 'Juridical person:', column: 'juridical_person', type: 'string', showOnBiobankCard: true },
   { label: 'Country:', column: 'country', type: 'string', showOnBiobankCard: true },
