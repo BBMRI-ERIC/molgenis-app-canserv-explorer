d1954ce4c043b84c0ae18449a433cb48d1f1056b
Konrad Lang <konrad.lang@bbmri-eric.eu>
2024-04-08 14:01:06 +0200

    Add maximum_number_of_units to initial collection columns.

--- src/config/initialCollectionColumns.js
+++ src/config/initialCollectionColumns.js
@@ -4,6 +4,7 @@ const initialCollectionColumns = [
   { label: 'Acronym:', column: 'acronym', type: 'string', showOnBiobankCard: true },
   { label: 'Description:', column: 'description', type: 'string', showOnBiobankCard: false },
   { label: 'Name:', column: 'name', type: 'string', showOnBiobankCard: false },
-  { label: 'Unit of access:', column: 'unit_of_access', type: 'string', showOnBiobankCard: true }]
+  { label: 'Unit of access:', column: 'unit_of_access', type: 'string', showOnBiobankCard: true },
+  { label: 'Maximum number of units:', column: 'maximium_number_of_units', type: 'string', showOnBiobankCard: true }]
 
 module.exports = initialCollectionColumns
