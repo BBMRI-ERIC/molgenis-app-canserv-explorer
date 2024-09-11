81aa328a48656f3a7ced13327c9e33cb213113e0
Konrad Lang <konrad.lang@bbmri-eric.eu>
2024-09-11 17:12:07 +0200

    Added with call id for ARIA: 'canserv-4th-challenge-driven-call'

--- src/components/popovers/NegotiatorSelection.vue
+++ src/components/popovers/NegotiatorSelection.vue
@@ -99,7 +99,6 @@
           class="btn btn-secondary ml-auto"
           @click="sendRequest4thChallengeCall">{{ negotiatorChallengeCall4ButtonText }}
         </b-button>
-        <!-- TODO: URL for 4th challenge driven call -->
         <button type="button" class="btn btn-primary" @click="openInNewTab('https://www.canserv.eu/calls/challenge-call-training-the-next-generation-of-cancer-researchers/')">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-square" viewBox="0 0 16 16">
                   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"></path>
